890b03a93953232000aea4c583c27f0003e866ff
Mananpoddar <119034136+manan652@users.noreply.github.com>
2024-06-29 22:10:22 +0530

    Create Array_insert_end.cpp

---
+++ Arrays/Array_insert_end.cpp
@@ -0,0 +1,7 @@
+//Back-end complete function Template for C++
+
+void insertAtEnd(int arr[],int sizeOfArray,int element)
+{
+    // just assign the element at last index(sizeOfArray-1) of the array.
+    arr[sizeOfArray-1]=element;
+}
