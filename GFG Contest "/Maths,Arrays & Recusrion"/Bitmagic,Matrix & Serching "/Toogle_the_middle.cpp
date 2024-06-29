ff2301b3a1cf7370d8d67f604b20f195e7eada03
mananpoddar32 <119005212+mananpoddar32@users.noreply.github.com>
2024-06-29 12:57:35 +0530

    Update Toogle_the_middle.cpp

--- GFG Contest "/Maths,Arrays & Recusrion"/Bitmagic,Matrix & Serching "/Toogle_the_middle.cpp
+++ GFG Contest "/Maths,Arrays & Recusrion"/Bitmagic,Matrix & Serching "/Toogle_the_middle.cpp
@@ -1,39 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
 
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// } Driver Code Ends
+//User function Template for C++
+
+int toggleTheMiddle(int n)
+{
+    //Your code here
+     if(n == 1) {
+        return 0;
+    } else if(n == 0) {
+        return 1;
+    } else {
+        int NumberOfBits = floor(log(n)/log(2)) + 1;
+        
+        if(!(NumberOfBits % 2)) {
+            // shift the middle and middle -1 bit
+            int Middle = NumberOfBits / 2;
+            int before_Middle = Middle - 1;
+            
+            n = (n ^ (1 << (Middle)));
+            n = (n ^ (1 << (before_Middle)));
+        } else {
+            // shift the middle bit only
+            int Middle = NumberOfBits / 2;
+            n = (n ^ (1 << (Middle)));
+        }
+        return n;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+
 int main() {
-	int T;
-	cin>>T;
-	
-	while(T--){
-	    
-	    int N;
-	    cin>>N;
-	    
-	    if(N == 1){
-	        cout<<"0"<<endl;
-	    }else if(N == 0){
-	        cout<<"1"<<endl;
-	    }else{
-	        int NumberOfBits = floor(log(N)/log(2)) + 1;
-	        
-	        if(!(NumberOfBits % 2)){
-	            //shift the middle and middle -1 bit
-	            int Middle = NumberOfBits/2;
-	            int before_Middle = Middle - 1;
-	            
-	            N = (N ^ (1 << (Middle)));
-	            N = (N ^ (1 << (before_Middle)));
-	            
-	            cout<<N<<endl;
-	        }else{
-	            //shift the middle bit only
-	            int Middle = NumberOfBits/2;
-	            N = (N ^ (1 << (Middle)));
-	            cout<<N<<endl;
-	        }
-	    }
-	}
+    int testcases;
+    cin>>testcases;
+    while(testcases--)
+    {
+        int n;
+        cin>>n;
+        
+        cout<<toggleTheMiddle(n)<<endl;
+        
+        
+    }
 	return 0;
 }
+// } Driver Code Ends
