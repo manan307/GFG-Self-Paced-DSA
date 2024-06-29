486d97a6b86302380b507453cde3bc0f99ee9cc3
mananpoddar32 <119005212+mananpoddar32@users.noreply.github.com>
2024-06-29 12:59:01 +0530

    Update Without_Adjacent.cpp

--- GFG Contest "/Maths,Arrays & Recusrion"/Bitmagic,Matrix & Serching "/Without_Adjacent.cpp
+++ GFG Contest "/Maths,Arrays & Recusrion"/Bitmagic,Matrix & Serching "/Without_Adjacent.cpp
@@ -1,34 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+long long arr[11000001];
 
 
-int main() {
-	
-	int T;
-	cin>>T;
-	
-	while(T--){
-	    
-	    int N;
-	    cin>>N;
-	    
-	    int Arr[N];
-	    
-	    for(int i=0; i<N; i++){
-	        cin>>Arr[i];
-	    }
-	    
-	    long long inclUDE = Arr[0];
-	    long long exclUDE = 0;
-	    long long new_excl;
-	    
-	    for(int i=1; i<N; i++){
-	        
-	        new_excl = (inclUDE > exclUDE) ? inclUDE : exclUDE;
-	        
-	        inclUDE = exclUDE  + Arr[i];
-	        exclUDE =  new_excl;
-	    }
-	    
-	    cout<<((inclUDE > exclUDE) ? inclUDE : exclUDE)<<endl;
-	}
-	return 0;
+// } Driver Code Ends
+//User function Template for C++
+
+long long FindMaxSum(long long arr[], int n) {
+    if (n == 0) return 0;
+    if (n == 1) return arr[0];
+
+    long long inclUDE = arr[0];
+    long long exclUDE = 0;
+    long long new_excl;
+
+    for (int i = 1; i < n; i++) {
+        new_excl = max(inclUDE, exclUDE);
+        inclUDE = exclUDE + arr[i];
+        exclUDE = new_excl;
+    }
+
+    return max(inclUDE, exclUDE);
 }
+
+//{ Driver Code Starts.
+
+
+/* Driver program to test above function */
+int main() 
+{ 
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        
+        cout<<FindMaxSum(arr,n)<<endl;
+    }
+return 0; 
+}
+// } Driver Code Ends
