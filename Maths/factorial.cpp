badd1fdea95825fbd5adec155d1ba7fa3705bb18
Mananpoddar <119034136+manan652@users.noreply.github.com>
2024-07-01 14:12:26 +0530

    Create factorial.cpp

---
+++ Maths/factorial.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+long long factorial(int);
+
+// } Driver Code Ends
+
+
+// User function Template for C++
+
+class Solution{
+    public:
+        // Complete this function
+    long long factorial(int N) {
+        // Your code her
+        if(N==0)
+        return 1;
+        return N * factorial(N-1);
+    }
+
+};
+
+
+
+//{ Driver Code Starts.
+int main() {
+    int T, N;
+    cin >> T; // input number of testcases
+    while (T--) {
+        cin >> N; // input n
+        Solution ob;
+        cout << ob.factorial(N) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
